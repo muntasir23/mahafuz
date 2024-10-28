2580e78e145d6539a8282ce8854eadcf449cbf8f
muntasir23 <muntasiraahmed3@gmail.com>
2024-10-28 03:35:45 -0700

    nav update

--- src/components/navbar/MobileNav.js
+++ src/components/navbar/MobileNav.js
@@ -29,69 +29,73 @@ export default function MobileNav() {
         </p>
       </div>
 
-      <div className="w-[100%] grid place-items-center overflow-hidden">
-        <div
-          className={`absolute  w-[95%] h-[90vh] top-[60px] bg-black rounded-md border border-zinc-800 ${
-            !show ? "menubar" : "menubar-open p-2 py-5"
-          }`}
-        >
-          <div className="w-full ">
-            <div>
-              <img alt="" src={logo} className="w-[60px]" />
+      {show ? (
+        <div className="w-[100%] grid place-items-center overflow-hidden">
+          <div
+            className={`absolute  w-[95%] h-[90vh] top-[60px] bg-black rounded-md border border-zinc-800 ${
+              !show ? "menubar" : "menubar-open p-2 py-5"
+            }`}
+          >
+            <div className="w-full ">
+              <div>
+                <img alt="" src={logo} className="w-[60px]" />
+              </div>
+              <ul className=" mt-10">
+                <li className="mt-2 border-b border-b-zinc-900 pb-2">
+                  <NavLink
+                    onClick={menuClose}
+                    to="/"
+                    className={`text-white hover:text-black font-semibold hover:bg-[#ffbd59] w-full inline-block rounded p-2 ${({
+                      isActive,
+                      isPending,
+                    }) => (isActive ? "active" : "")}`}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="mt-2 border-b border-b-zinc-900 pb-2">
+                  <NavLink
+                    onClick={menuClose}
+                    to="/projects"
+                    className={`text-white hover:text-black font-semibold hover:bg-[#ffbd59] w-full inline-block rounded p-2 ${({
+                      isActive,
+                      isPending,
+                    }) => (isActive ? "active" : "")}`}
+                  >
+                    Projects
+                  </NavLink>
+                </li>
+                <li className="mt-2 border-b border-b-zinc-900 pb-2">
+                  <NavLink
+                    onClick={handleMenu}
+                    to="/allplans"
+                    className={`text-white hover:text-black font-semibold hover:bg-[#ffbd59] w-full inline-block rounded p-2 ${({
+                      isActive,
+                      isPending,
+                    }) => (isActive ? "active" : "")}`}
+                  >
+                    Price & Plans
+                  </NavLink>
+                </li>
+                <li className="mt-2 border-b border-b-zinc-900 pb-2">
+                  <NavLink
+                    onClick={handleMenu}
+                    to="/blogs"
+                    className={`text-white hover:text-black font-semibold hover:bg-[#ffbd59] w-full inline-block rounded p-2 ${({
+                      isActive,
+                      isPending,
+                    }) => (isActive ? "active" : "")}`}
+                  >
+                    Blogs
+                  </NavLink>
+                </li>
+              </ul>
             </div>
-            <ul className=" mt-10">
-              <li className="mt-2 border-b border-b-zinc-900 pb-2">
-                <NavLink
-                  onClick={menuClose}
-                  to="/"
-                  className={`text-white hover:text-black font-semibold hover:bg-[#ffbd59] w-full inline-block rounded p-2 ${({
-                    isActive,
-                    isPending,
-                  }) => (isActive ? "active" : "")}`}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="mt-2 border-b border-b-zinc-900 pb-2">
-                <NavLink
-                  onClick={menuClose}
-                  to="/projects"
-                  className={`text-white hover:text-black font-semibold hover:bg-[#ffbd59] w-full inline-block rounded p-2 ${({
-                    isActive,
-                    isPending,
-                  }) => (isActive ? "active" : "")}`}
-                >
-                  Projects
-                </NavLink>
-              </li>
-              <li className="mt-2 border-b border-b-zinc-900 pb-2">
-                <NavLink
-                  onClick={handleMenu}
-                  to="/allplans"
-                  className={`text-white hover:text-black font-semibold hover:bg-[#ffbd59] w-full inline-block rounded p-2 ${({
-                    isActive,
-                    isPending,
-                  }) => (isActive ? "active" : "")}`}
-                >
-                  Price & Plans
-                </NavLink>
-              </li>
-              <li className="mt-2 border-b border-b-zinc-900 pb-2">
-                <NavLink
-                  onClick={handleMenu}
-                  to="/blogs"
-                  className={`text-white hover:text-black font-semibold hover:bg-[#ffbd59] w-full inline-block rounded p-2 ${({
-                    isActive,
-                    isPending,
-                  }) => (isActive ? "active" : "")}`}
-                >
-                  Blogs
-                </NavLink>
-              </li>
-            </ul>
           </div>
         </div>
-      </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
